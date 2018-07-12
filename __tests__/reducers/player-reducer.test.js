5ba81a7f7bd35ea2abdff4b53f3e1f69044ef224
Brenda Franco and Kayl Eubanks <brenda@>
2018-07-12 15:24:16 -0700

    Add test to initiate player state.

--- __tests__/reducers/player-reducer.test.js
+++ __tests__/reducers/player-reducer.test.js
@@ -1,10 +1,20 @@
 import playerReducer from './../../src/reducers/player-reducer';
 import c from './../../src/constants';
 
+const playerState = {
+  player: true
+};
+
 describe('playerReducer', () => {
 
   test('Should return default player state', () => {
     expect(playerReducer({}, {type:null})).toEqual({});
+  });
+
+  test('Should initiate player state', () => {
+    expect(playerReducer(playerState, { type: c.PLAYER_TOGGLE})).toEqual({
+      player: false
+    });
   });
 
 });
